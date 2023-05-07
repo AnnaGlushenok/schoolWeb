09935c16f9978770e422ae34fc61c07ada44438d
AnnaGlushenok <annasan14052003@gmail.com>
2023-05-07 21:27:43 +0300

    test_teacher

--- pages/test_teacher/test_teacher.js
+++ pages/test_teacher/test_teacher.js
@@ -1,8 +1,13 @@
 function addDivBlock() {
-    let id = findLastId(".questions");
+    // let id = findLastId(".questions");
+    let id = findLastId(".block__card");
+    console.log("id ", id);
     let div = document.createElement("div")
     div.setAttribute("class", "questions__question question");
-    div.setAttribute("id", (id + 1));
+    let len = document.querySelector(".block__card").children.length - 2;
+    console.log("len ", len);
+    div.setAttribute("id", "" + id + len);
+    //  document.querySelector(".block__card").children.length - 2;
 
     return div;
 }
@@ -54,6 +59,85 @@ function addQuestion() {
     div.appendChild(input);
     div.appendChild(button);
 
-    let parent = document.getElementsByClassName("questions__add_btn")[0];
-    document.getElementsByClassName("questions")[0].insertBefore(div, parent);
+    let id = event.target.parentNode.parentNode.parentNode.id;
+    let parent = document.getElementById(id).children[1];
+    let d = document.getElementById(id);
+    d.insertBefore(div, parent);
+}
+
+function addCard() {
+    let div = document.createElement("div");
+    div.setAttribute("class", "block__card card");
+    div.setAttribute("id", findLastId(".block") + 1);
+
+    let questionDiv = document.createElement("div");
+    questionDiv.setAttribute("class", "card__question_title question_title");
+
+    let input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("class", "question_title__question");
+    input.setAttribute("name", "question");
+    input.setAttribute("placeholder", "Вопрос");
+
+    let select = document.createElement("select");
+    select.setAttribute("name", "question_types");
+    select.setAttribute("class", "question_title__combobox");
+    select.setAttribute("id", "question_type");
+
+    let values = ["1 выбор", "Чек боксы", "Самому написать"];
+    for (let i = 0; i < values.length; i++) {
+        let option = document.createElement("option");
+        option.innerHTML = values[i]
+        select.appendChild(option);
+    }
+
+    let buttonsDiv = document.createElement("div");
+    buttonsDiv.setAttribute("class", "card__questions questions");
+
+    let button = document.createElement("button");
+    button.setAttribute("class", "questions__add_btn");
+    button.setAttribute("onclick", "addQuestion()");
+
+    let img = document.createElement("img");
+    img.setAttribute("class", "questions__img");
+    img.setAttribute("src", "../../images/plus.svg");
+    img.setAttribute("alt", "plus");
+
+    questionDiv.appendChild(input);
+    questionDiv.appendChild(select);
+    button.appendChild(img);
+    buttonsDiv.appendChild(button);
+
+    div.appendChild(questionDiv);
+    div.appendChild(buttonsDiv);
+
+    let parent = document.getElementsByClassName("block__button")[0];
+    document.getElementsByClassName("block")[0].insertBefore(div, parent);
+}
+
+function save() {
+    let children = document.querySelector(".page").children;
+    let data = [];
+    let ids = [];
+    for (let i = 0; i < children.length; i++) {
+        data[i] = document.getElementsByClassName("block__content")[0].textContent;
+        ids[i] = children[i].id;
+    }
+    console.log(data);
+    let json = {
+        ids: ids,
+        content: data
+    }
+    console.log(json);
+    fetch('/api/user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(json)
+    }).then(function (response) {
+        console.log("Данные успешно отправлены на сервер");
+    }).catch(function (error) {
+        console.log("Ошибка при отправке данных на сервер");
+    });
 }
